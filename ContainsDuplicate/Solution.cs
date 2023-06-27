f19646a9b267f3d7a3c59d56d0f655a3be847622
Chinenye Okeke <maryroseokeke@yahoo.com>
2023-06-27 01:26:02 +0100

    Add project files.

---
+++ ContainsDuplicate/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace ContainsDuplicate
+{
+    public static class Solution
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> NumberSet = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+                if (NumberSet.Contains(num))
+                
+                    return true;
+
+                    NumberSet.Add(num);
+                
+            }
+            return false;
+        }
+    }
+}
