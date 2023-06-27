de609ea51edf8cf679be66df450db76eda007c6b
Chinenye Okeke <maryroseokeke@yahoo.com>
2023-06-27 21:48:53 +0100

    Cleaned up the program class

--- ContainsDuplicate/Solution.cs
+++ ContainsDuplicate/Solution.cs
@@ -17,5 +17,26 @@
             }
             return false;
         }
+
+        public static void displayResult()
+        {
+            //Case 1
+            int[] input1 = { 1, 2, 3, 1 };
+            var output1 = Solution.ContainsDuplicate(input1);
+
+            Console.WriteLine($"Output: {output1}");
+
+            //Case 2
+            int[] input2 = { 1, 2, 3, 4 };
+            var output2 = Solution.ContainsDuplicate(input2);
+
+            Console.WriteLine($"Output: {output2}");
+
+            //Case 3
+            int[] input3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+            var output3 = Solution.ContainsDuplicate(input3);
+
+            Console.WriteLine($"Output: {output3}");
+        }
     }
 }
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -6,23 +6,7 @@ namespace Presentation
     {
         static void Main(string[] args)
         {
-            //Case 1
-            int[] input1 = { 1, 2, 3, 1};
-            var output1 = Solution.ContainsDuplicate(input1);
-
-            Console.WriteLine($"Output: {output1}");
-
-            //Case 2
-            int[] input2 = { 1, 2, 3, 4 };
-            var output2 = Solution.ContainsDuplicate(input2);
-
-            Console.WriteLine($"Output: {output2}");
-
-            //Case 3
-            int[] input3 = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
-            var output3 = Solution.ContainsDuplicate(input3);
-
-            Console.WriteLine($"Output: {output3}");
+            Solution.displayResult();
         }
     }
     
